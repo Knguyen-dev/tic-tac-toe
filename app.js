b1f1c234162f422fec609e510734952312019d52
Kevin Nguyen <thenoc27@gmail.com>
2023-01-13 19:23:49 -0500

    Finished the basic styling and html structure of the page

---
+++ app.js
@@ -0,0 +1,27 @@
+/*
+- Store gameboard as an array inside Gameboard object. Players are also going to be stored
+in objects, and have an object to control hte flow of the game itself.
+
+- Have a little global code as possible. If you only need one of something such as 
+a gameboard, or displayController, then use a module. If you need multiple of something,
+such as players, create them with factories
+
+- Write a function that renders the contents of the gameboard array, filling the 
+arrays with things such as x's and o's
+- Writes functions that let players add marks to specific spots on the board, then link
+it to the DOM, letting the players click on spots on the boards. Don't forget to check
+if a spot on the board was already used.
+
+- Of course spend time first to think about where each function would go so that it all fits together
+
+- Write function or logic that checks when the game is over. Checks for when it's a 3 in a row (win) or 
+a tie
+
+- Let the players put htier names, have a button to already start/restart the game
+- Add a display element that congratulates the winning player 
+
+- Optional Challenge: Create an AI so that hte player can play against computer
+1. Start with having it make simple moves
+2. Work on making it smart, you can create an AI using minimix algorithm. You should google 
+this one.
+*/

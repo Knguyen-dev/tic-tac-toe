2ae37a070c303457104988e715175569d5002f27
Kevin Nguyen <thenoc27@gmail.com>
2023-01-14 19:57:34 -0500

    Created functionality for the reset button to reset the game. Refactored code to include more functions to reduce repetition and redundancy. Planning to create the game result modal, trying to fit more elements/variables in the objects, will try to implement functionality so that the game state and data persists after refreshing.

--- app.js
+++ app.js
@@ -1,37 +1,20 @@
 /*
-- Store gameboard as an array inside Gameboard object. Players are also going to be stored
-in objects, and have an object to control hte flow of the game itself.
-
-- Have a little global code as possible. If you only need one of something such as 
-a gameboard, or displayController, then use a module. If you need multiple of something,
-such as players, create them with factories
-
-- Write a function that renders the contents of the gameboard array, filling the 
-arrays with things such as x's and o's
-- Writes functions that let players add marks to specific spots on the board, then link
-it to the DOM, letting the players click on spots on the boards. Don't forget to check
-if a spot on the board was already used.
-
-- Write function or logic that checks when the game is over. Checks for when it's a 3 in a row (win) or 
-a tie
-- have a button to already start/restart the game
-- Add a display element that congratulates the winning player 
-
-- Optional Challenge: Create an AI so that hte player can play against computer
-1. Start with having it make simple moves
-2. Work on making it smart, you can create an AI using minimix algorithm. You should google 
-this one.
+- Create the game result modal with html and css
+- Then the step after that would be just putting all of the dom elements in grid Info, renaming grid info as docElements or something
+- Continue to try to fit stuff in objects and modules
 */
 
-// Extra DOM Elements
+// Extra DOM Elements that aren't related to the grid itself
 const playerXEl = document.getElementById("player-x");
 const playerOEl = document.getElementById("player-o");
 const scoreElements = document.querySelectorAll(".player-score-el");
 const beginGameBtn = document.getElementById("begin-game-btn");
+const resetGameBtn = document.getElementById("reset-game-btn");
 const alertEl = document.querySelector(".alert-el");
 
+// Object contains the main grid and the grid squares. The function also creates renders the grid squares and creates eventlisteners for them
 /*
-- Function creates and renders the tic-tac-toe squaresk, and adds event listeners for them
+- Function creates and renders the tic-tac-toe squares, and adds event listeners for them
 - Then returns an object with properties main grid, and the grid squares, which allows you
 to access those dom elements by accessing the gridInfo objet.
 */
@@ -50,78 +33,130 @@ const gridInfo = (() => {
   });
   return { playingGrid, gridSquares };
 })();
-
-// Player object factory
+// Player object factory that creates Player objects
 const Player = (id) => {
   let score = 0;
   return { id, score };
 };
-
+/*
+- board is an array that represents tictactoe board with positions or indices. A 0 value means no one has played that spot yet, hence the board always starts out with all zeroes
+to indicate that all spaces are free to be played:
+{
+  0, 1, 2,
+  3, 4, 5,
+  6, 7, 8
+}
+- currentPlayer is the player who's turn it currently is
+*/
 let gameInfo = (() => {
   let board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
   let playerX = Player("x");
   let playerO = Player("o");
   let currentPlayer = playerX;
-  const checkGame = () => {
-    let gameResult = {
-      finished: false,
-      is_tie: true,
-    };
-    let remainingSpots;
-
-    // Horizontal row check to see if a player has won
-    // board[i] !== 0, makes sure we are only checking when rows (or columns) have been marked by players
-    for (let i = 0; i < 9; i += 3) {
-      if (board[i] !== 0) {
-        if (board[i] === board[i + 1] && board[i] === board[i + 2]) {
-          gameResult.finished = true;
-          gameResult.is_tie = false;
-        }
-      }
-    }
-
-    // Vertical/column check to see if a player has won
-    for (let i = 0; i < 3; i++) {
-      if (board[i] !== 0) {
-        if (board[i] === board[i + 3] && board[i] === board[i + 6]) {
-          gameResult.finished = true;
-          gameResult.is_tie = false;
-        }
-      }
-    }
-
-    // Checking the diagonals to see if a player has won
-    // From top left to bottom right
-    if (board[0] !== 0) {
-      if (board[0] === board[4] && board[4] === board[8]) {
-        gameResult.finished = true;
-        gameResult.is_tie = false;
-      }
-    } else if (board[2] !== 0) {
-      if (board[2] === board[4] && board[4] === board[6]) {
-        gameResult.finished = true;
-        gameResult.is_tie = false;
-      }
-    }
-
-    // Checks the remaining spots, if there are 0 unused spots left, that means the game
-    // is finished and over, BUT the game is a tie, hence gameResult.is_tie is not changed to false
-    remainingSpots = board.filter((sqr) => {
-      return sqr === 0;
-    });
-
-    // If there are no remaining spots left, then the game is finished, but it isn't a tie
-    // This is in contrast to the above conditionals that test for a win
-    if (remainingSpots.length === 0) {
-      gameResult.finished = true;
-    }
-
-    return gameResult;
+  let gameResult = {
+    finished: false,
+    is_tie: true,
   };
-
-  return { board, playerX, playerO, currentPlayer, checkGame };
+  return { board, playerX, playerO, currentPlayer, gameResult };
 })();
+// Function toggles all of the grid squares, if parameter is true then we enable all of the squares, else disable all of them.
+function toggleGridSquares(is_enabled) {
+  if (is_enabled) {
+    gridInfo.gridSquares.forEach((sqr) => {
+      sqr.classList.remove("square-disabled");
+    });
+  } else {
+    gridInfo.gridSquares.forEach((sqr) => {
+      sqr.classList.add("square-disabled");
+    });
+  }
+}
+// Appears and disables the score elements, elements parameter are the score elements, is_visible will be a boolean that decides if we show the score elements
+function toggleScoreElements(is_visible) {
+  if (is_visible) {
+    scoreElements.forEach((scoreEl) => {
+      scoreEl.classList.remove("content-hidden");
+    });
+  } else {
+    scoreElements.forEach((scoreEl) => {
+      scoreEl.classList.add("content-hidden");
+    });
+  }
+}
+// Updates the text contained by the two score elements to reflect the scores of the players in the gameInfo object
+function updateScoreElements() {
+  scoreElements.forEach((scoreEl) => {
+    if (scoreEl.dataset.id === "x") {
+      scoreEl.textContent = gameInfo.playerX.score;
+    } else {
+      scoreEl.textContent = gameInfo.playerO.score;
+    }
+  });
+}
 
+// Function displays alerts, message parameter is the text that will be alerted to the player
+function displayAlert(message) {
+  alertEl.textContent = message;
+  alertEl.classList.remove("content-hidden");
+  setTimeout(() => {
+    alertEl.classList.add("content-hidden");
+  }, 3000);
+}
+function clearGrid() {
+  gridInfo.gridSquares.forEach((sqr) => {
+    const squareContentEl = sqr.querySelector(".square-content");
+    sqr.classList.remove("show-square-content"); // remove class for a smooth disappearance
+    squareContentEl.textContent = "";
+  });
+}
+// Function checks the board array in the gameInfo object to see if someone has won the game
+// Returns gameResult object in the gameInfo object
+function checkGame() {
+  let emptySpots;
+  const board = gameInfo.board;
+
+  // Checks the rows/horizontal
+  for (let i = 0; i < 9; i += 3) {
+    if (board[i] !== 0) {
+      if (board[i] === board[i + 1] && board[i] === board[i + 2]) {
+        gameInfo.gameResult.finished = true;
+        gameInfo.gameResult.is_tie = false;
+      }
+    }
+  }
+  // Checks columns/vertical
+  for (let i = 0; i < 3; i++) {
+    if (board[i] !== 0) {
+      if (board[i] === board[i + 3] && board[i] === board[i + 6]) {
+        gameInfo.gameResult.finished = true;
+        gameInfo.gameResult.is_tie = false;
+      }
+    }
+  }
+  // Checks diagonal from top left to bottom right
+  if (board[0] !== 0) {
+    if (board[0] === board[4] && board[4] === board[8]) {
+      gameInfo.gameResult.finished = true;
+      gameInfo.gameResult.is_tie = false;
+    }
+  }
+  // Checks diagonal from top right to bottom left
+  if (board[2] !== 0) {
+    if (board[2] === board[4] && board[4] === board[6]) {
+      gameInfo.gameResult.finished = true;
+      gameInfo.gameResult.is_tie = false;
+    }
+  }
+  // Gets all of the empty spots left. If there are no empty spots then the game is finished. This helps handles ties, as the above conditionals only activate on wins, but if no one wins
+  // we can check that there are no spots left, and so we can say the game has finished. This leaves the tie boolean as true, hence a tie game.
+  emptySpots = board.filter((sqr) => {
+    return sqr === 0;
+  });
+  if (emptySpots.length === 0) {
+    gameInfo.gameResult.finished = true;
+  }
+  return gameInfo.gameResult;
+}
 // Gets the index (position on the grid that player wants to hit).
 // If the position on the grid has value 0, then that spot hasn't been marked yet, which makes it a valid play
 // Else if the position doesn't have value 0, that means it has either value 'x' or 'o', which means it's already been played so it is invalid
@@ -132,8 +167,7 @@ function validateMove(index) {
   }
   return is_valid;
 }
-
-// This function changes the current player visually and in the logic
+// This function changes the current player visually and in the logic; player parameter represents the player that just marked the board, so it will be the turn of the other player
 function changePlayerTurn(player) {
   if (player.id === "x") {
     gameInfo.currentPlayer = gameInfo.playerO;
@@ -145,94 +179,109 @@ function changePlayerTurn(player) {
     playerOEl.classList.remove("highlight-element");
   }
 }
-
+// Function is responsible for marking the board when the players click on various sections of the grid, event is needed to get the part of the grid that was clicked
 function markBoard(e) {
   const { currentPlayer } = gameInfo;
   const currentSquare = e.currentTarget;
   const squareContentEl = currentSquare.querySelector(".square-content");
   const squareIndex = currentSquare.dataset.position;
 
-  // If the move is valid then mark hte board then switch turns
+  // If the move is valid
   if (validateMove(squareIndex)) {
-    // Mark the board, then add the class so that the mark appears
-    // Then disable the clicked square on the grid because it's already been marked
-    squareContentEl.textContent = currentPlayer.id;
-    squareContentEl.classList.add("show-square-content");
+    squareContentEl.textContent = currentPlayer.id; // puts mark on the dom element
+    gameInfo.board[squareIndex] = currentPlayer.id; // marks the board array
+    squareContentEl.classList.add("show-square-content"); // shows the mark and disables square
     currentSquare.classList.add("square-disabled");
 
-    // Put player mark on the corresponding position in the array
-    gameInfo.board[squareIndex] = currentPlayer.id;
-
-    // Check the game state to see if anyone won yet or if the game has finished
-    const result = gameInfo.checkGame();
-
+    const result = checkGame(); // Check the game state to see if anyone won yet or if the game has finished
     if (result.finished) {
       endGame(currentPlayer, result.is_tie);
     } else {
-      // If the game isnt finished then
-      // Call a function to make it so the it's the other player's turn
-      console.log("Changed player triggered");
       changePlayerTurn(currentPlayer);
     }
   } else {
+    // If the move that was played isn't valid, then display an alert
     displayAlert("Hey that move was invalid! Obviously that spot was already taken.");
   }
 }
 
-// Function displays alerts
-function displayAlert(message) {
-  alertEl.textContent = message;
-  alertEl.classList.remove("content-hidden");
-  setTimeout(() => {
-    alertEl.classList.add("content-hidden");
-  }, 3000);
-}
-
 // Function is called to display end message
+// Player is the player who won the game
 function endGame(player, is_tie_game) {
-  let message = "";
+  // If it's a tie then show the message that it's a tie, else do other logic
   if (is_tie_game) {
     console.log(`Tie game triggered, tie game boolean: ${is_tie_game}`);
   } else {
-    console.log(`Player ${player.id} won the game!`);
+    // Increase the score of the winning player
+    if (player.id === "x") {
+      gameInfo.playerX.score += 1;
+    } else {
+      gameInfo.playerO.score += 1;
+    }
   }
 
-  // Reset the board array since the game is over, in preparation for the next game
-  // Make current player x so that x has the first turn on the first one
+  // update the text for the scores
+  updateScoreElements();
+
+  // Partially reset the game so that the players can play another round
+  // Disables the grid squares
+  toggleGridSquares(false);
+
+  // Reset the game info that affects the rounds
+  setupNewRound();
+}
+
+function setupNewRound() {
+  // Clears round data from the past
   gameInfo.board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
   gameInfo.currentPlayer = gameInfo.playerX;
+  gameInfo.gameResult.finished = false;
+  gameInfo.gameResult.is_tie = true;
 
-  // Have a window that shows a winning message, obviously need to be able to exit out of the
-  // that window
-  // At the end we want to reset the start button, reset the text and
-
+  // Sets up the button for a new round visually and makes it interactive
   beginGameBtn.classList.remove("highlight-element");
   beginGameBtn.textContent = "Start Game";
+  beginGameBtn.disabled = false;
 }
 
 /*
 - function should know who the current player is by accessing the currentPlayer property
 */
 function startGame() {
+  // Indicates the begin game element has been clicked
   beginGameBtn.classList.add("highlight-element");
   beginGameBtn.textContent = "Game Ongoing";
-  playerXEl.classList.add("highlight-element");
-
-  // Visually clear the board so that information from the last game doesn't persist
-  gridInfo.gridSquares.forEach((sqr) => {
-    const squareContentEl = sqr.document.querySelector(".");
-    squareContentEl.textContent = "";
-  });
+  playerXEl.classList.add("highlight-element"); //Starts with player X being indicated as the one who's starting the game
+  playerOEl.classList.remove("highlight-element");
 
   // Shows the scores:
-  scoreElements.forEach((scoreEl) => {
-    scoreEl.classList.remove("content-hidden");
-  });
-
-  // Make the squares clickable
-  gridInfo.gridSquares.forEach((sqr) => {
-    sqr.classList.remove("square-disabled");
-  });
+  toggleScoreElements(true);
+  //Visually clear the board so that information from the last game doesn't persist
+  clearGrid();
+  // Enable the grid squares
+  toggleGridSquares(true);
+  // At the end we disable the button
+  beginGameBtn.disabled = true;
 }
 
 beginGameBtn.addEventListener("click", startGame);
+
+// Let's say the resetGameBtn can be clicked at anytime, not just at the end of a round
+/*
+- Unindicates the players
+- Deletes round data and sets up a new round
+- Hides player scores and resets them
+- Makes the grid squares not interactive
+- Clears the grid of past marks
+*/
+resetGameBtn.addEventListener("click", () => {
+  playerXEl.classList.remove("highlight-element");
+  playerOEl.classList.remove("highlight-element");
+  setupNewRound();
+  toggleScoreElements(false);
+  gameInfo.playerX.score = 0;
+  gameInfo.playerO.score = 0;
+  updateScoreElements();
+  toggleGridSquares(false);
+  clearGrid();
+});
